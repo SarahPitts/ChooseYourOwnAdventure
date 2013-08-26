238d1909b395ec92cb0a07843e35a21f719ac686
Sarah Pitts <mccallsmedia@gmail.com>
2013-08-26 15:13:34 -0500

    first commit of Choose Your Own Adventure

---
+++ script.js
@@ -0,0 +1,31 @@
+// Check if the user is ready to play!
+
+confirm("I am ready to play!");
+
+var age = prompt("What's your age?");
+if( age < 18 ){
+   alert("Allowed to play but we take no responsibility");
+}
+else{
+   alert("Play On!");
+}
+
+alert("Snow White and Batman were hanging out at the bus stop, waiting to go to the shops. There was a sale on and both needed some new threads. You've never really liked Batman. You walk up to him.")
+
+alert("Batman glares at you.")
+
+var userAnswer = prompt("Are you feeling lucky, punk?")
+if( userAnswer == "yes" ){
+   alert("Batman hits you very hard. It's Batman and you're you! Of course Batman wins!");
+}
+else{
+   alert("Good choice! You are a winner in the game of not getting beaten up by Batman.");
+}
+
+var feedback = prompt("rate this game out of 10")
+if( feedback > "8" ){
+   alert("This is just the beginning of my game empire. Stay tuned for more!");
+}
+else{
+   alert("I slaved away at this game and you gave me that score?! The nerve! Just you wait!");
+}
